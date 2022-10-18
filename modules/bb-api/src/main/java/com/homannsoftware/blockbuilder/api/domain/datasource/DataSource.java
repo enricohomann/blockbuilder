8ac6488a4030a81fe042f7d93da7093a15328e4a
Enrico Homann <enrico.homann@homannsoftware.com>
2022-10-18 23:36:51 +0200

    bb-api: Create Model #1

--- modules/bb-api/src/main/java/com/homannsoftware/blockbuilder/api/domain/datasource/DataSource.java
+++ modules/bb-api/src/main/java/com/homannsoftware/blockbuilder/api/domain/datasource/DataSource.java
@@ -1,4 +1,9 @@
 package com.homannsoftware.blockbuilder.api.domain.datasource;
 
 public interface DataSource {
+
+    default String getName() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
 }
