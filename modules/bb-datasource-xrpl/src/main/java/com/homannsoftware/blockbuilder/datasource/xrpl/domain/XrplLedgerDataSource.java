99dbf0a3034fedfa43db12775ebbae9455e18860
Enrico Homann <enrico.homann@homannsoftware.com>
2022-10-19 12:31:09 +0200

    Implement bb-datasource-xrpl#3

--- modules/bb-datasource-xrpl/src/main/java/com/homannsoftware/blockbuilder/datasource/xrpl/domain/XrplLedgerDataSource.java
+++ modules/bb-datasource-xrpl/src/main/java/com/homannsoftware/blockbuilder/datasource/xrpl/domain/XrplLedgerDataSource.java
@@ -3,4 +3,9 @@ package com.homannsoftware.blockbuilder.datasource.xrpl.domain;
 import com.homannsoftware.blockbuilder.api.domain.datasource.ledger.LedgerDataSource;
 
 public class XrplLedgerDataSource implements LedgerDataSource {
+
+    @Override
+    public String getName() {
+        return XrplLedgerDataSource.class.getSimpleName();
+    }
 }
